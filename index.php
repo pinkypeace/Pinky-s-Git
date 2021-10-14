16e742a72a7c4f7940592f27daaa12f784809451
= <=>
2021-10-14 16:19:58 +0100

    wahala be like pinky push

--- index.php
+++ index.php
@@ -9,5 +9,15 @@
 	i dont want again
 	i dont like gmail
 </header>
+	<form method="post">
+		<input type="date" placeholder="choose date" name="who"></input>
+		<button name="why"> DO NOT</button>
+	</form>
+	<?php if (isset($_POST['why'])): 
+	 $var = $_POST['who'] . "     ".time();
+		 echo $var;
+		 ?>
+
+		<?php endif ?>
 </body>
 </html>
